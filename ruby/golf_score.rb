121993c0bd6a25dd3c5753a00d4f9571532b6537
pado <mumwork202003@gmail.com>
2023-09-27 16:10:10 +0900

    コードレビューの修正

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,30 +1,26 @@
 input = gets.chomp.split(',').map(&:to_i)
 input2 = gets.chomp.split(',').map(&:to_i)
 output = []
+
+SCORE_LIST = {
+  1 => "ボギー",
+  0 => "パー",
+  -1 => "バーディー",
+  -2 => "イーグル",
+  -3 => "アルバトロス",
+  -4 => "コンドル",
+}
+
 input.zip(input2).map do |x, y|
   score = y - x
 
-  output << if x == 3 && y == 1
-              'ホールインワン'
-            elsif x == 4 && y == 1
-              'ホールインワン'
-            elsif x == 5 && y == 2
-              'アルバトロス'
-            elsif x == 5 && y == 1
-              'コンドル'
-            elsif x == 4 && y == 2
-              'イーグル'
-            elsif x == 5 && y == 3
-              'イーグル'
-            elsif score.zero?
-              'パー'
-            elsif score == -1
-              'バーディ'
-            elsif score == 1
-              'ボギー'
-            else
-              "#{score}ボギー"
-            end
+  output << if (3..4).cover?(x) && y == 1
+    'ホールインワン'
+  elsif score >= 2
+    "#{score}ボギー"
+  else
+    SCORE_LIST[score]
+  end
 end
 
 puts output.join(',')
